18e719b7c74796fe0956156f4fd238d1657592f5
Mezuno <82967817+Mezuno@users.noreply.github.com>
2023-03-30 10:47:55 +0300

    Modal windows for deleting
    Modal windows for deleting users and products

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('adminlte/css/admin.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
