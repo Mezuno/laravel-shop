509b03ba8336b906b8bb9379bbe5f7ce7c6b2b8c
Mezuno <82967817+Mezuno@users.noreply.github.com>
2023-03-30 16:04:28 +0300

    Add cart client view page
    Add cart client view page

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,27 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/main/index.vue')
-    },
-    {
-      path: '/products',
-      name: 'products.index',
-      component: () => import('../views/products/index.vue')
-    },
-    {
-      path: '/products/:id',
-      name: 'products.show',
-      component: () => import('../views/products/show.vue')
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL), routes: [{
+        path: '/', name: 'index', // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../views/main/index.vue')
+    }, {
+        path: '/products', name: 'products.index', component: () => import('../views/products/index.vue')
+    }, {
+        path: '/products/:id', name: 'products.show', component: () => import('../views/products/show.vue')
+    }, {
+        path: '/cart', name: 'cart', component: () => import('../views/cart.vue')
+    },]
 })
 
 export default router
---
+++ resources/js/views/cart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "cart"
+}
+</script>
+
+<style scoped>
+
+</style>
