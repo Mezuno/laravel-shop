920d14a5ed4a2c71f89188e3c1e95795b45cf699
Mezuno <82967817+Mezuno@users.noreply.github.com>
2023-04-22 12:00:21 +0300

    [HotFix] Product show unpublished products
    [HotFix] Product show unpublished products

--- app/Http/API/Controllers/Product/ShowController.php
+++ app/Http/API/Controllers/Product/ShowController.php
@@ -5,11 +5,15 @@ namespace App\Http\API\Controllers\Product;
 use App\Entities\Product\Http\Resources\ProductResource;
 use App\Entities\Product\Models\Product;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
 
 class ShowController extends Controller
 {
     public function __invoke(Product $product)
     {
+        if (!$product->published_at) {
+            return new Response('Товар не существует или не опубликован', 404);
+        }
         return new ProductResource($product);
     }
 }
